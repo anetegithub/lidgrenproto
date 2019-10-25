719aa132899ff5c5be2a5e56695f2993affcd9d4
a.tretyakov <a.tretyakov@bars-open.ru>
2019-10-25 10:59:11 +0300

    Add project files.

---
+++ lidgrenexample.client/Program.cs
@@ -0,0 +1,52 @@
+﻿using Lidgren.Network;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lidgrenexample.client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new NetPeerConfiguration("application name");
+            var client = new NetClient(config);
+            client.Start();
+            client.Connect(host: "127.0.0.1", port: 12345);
+
+            ThroughtThread(client);
+
+            string input = string.Empty;
+            while (input!="exit")
+            {
+                input = Console.ReadLine();
+                var message = client.CreateMessage();
+                message.Write(input);
+                client.SendMessage(message, NetDeliveryMethod.ReliableOrdered);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void ThroughtThread(NetClient client)
+        {
+            new Thread(() =>
+            {
+                client.MessageReceivedEvent.WaitOne(); // this will block until a message arrives
+                GotMessage(client);
+            }).Start();
+        }
+
+        private static void ThroughCallback(NetClient client)
+        {
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            client.RegisterReceivedCallback(new System.Threading.SendOrPostCallback(GotMessage));
+        }
+
+        public static void GotMessage(object peer)
+        {
+            var message = (peer as NetClient).ReadMessage();
+            Console.WriteLine(message.ReadString());
+        }
+    }
+}
---
+++ lidgrenexample/Program.cs
@@ -0,0 +1,51 @@
+﻿using Lidgren.Network;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lidgrenexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new NetPeerConfiguration("application name")
+            { Port = 12345 };
+            var server = new NetServer(config);
+            server.Start();
+
+            ThroughtThread(server);
+            
+            string input = string.Empty;
+            while (input != "exit")
+            {
+                input = Console.ReadLine();
+                var message = server.CreateMessage();
+                message.Write(input);
+                server.SendMessage(message, server.Connections, NetDeliveryMethod.ReliableOrdered,0);
+            }
+        }
+
+
+        private static void ThroughtThread(NetServer client)
+        {
+            new Thread(() =>
+            {
+                client.MessageReceivedEvent.WaitOne(); // this will block until a message arrives
+                GotMessage(client);
+            }).Start();
+        }
+
+        private static void ThroughCallback(NetServer client)
+        {
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            client.RegisterReceivedCallback(new System.Threading.SendOrPostCallback(GotMessage));
+        }
+
+        public static void GotMessage(object peer)
+        {
+            var message = (peer as NetServer).ReadMessage();
+            Console.WriteLine(message.ReadString());
+        }
+    }
+}
